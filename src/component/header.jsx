323bdaeb9445b12f91e530bac8bc2da2aaf0ad9e
John Mutua <mutubenedict920@gmail.com>
2023-11-24 06:51:33 +0300

    Modified meme.jsx file

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -4,7 +4,7 @@ import styles from "../style.module.css";
 function Header(){
     return(<div className={styles.nav}>
         <img src={Image1} className={styles.pic}/>
-        <h2 className={styles.title1}>ImageGenerator</h2>
+        <h2 className={styles.title1}>MemeGenerator</h2>
         <h3 className={styles.title2}>React Course - project 3</h3>
     </div>)
 }
--- src/component/meme.jsx
+++ src/component/meme.jsx
@@ -19,14 +19,30 @@ function Meme(){
         }))
        
     }
-    const[allMemeImages,setMemeImages] = useState(Memesdata.data.memes)
+    function handlechange(event){
+        const {name,value} = event.target
+        setmeme(prevState => ({
+            ...prevState,
+            [name] : value
+        }))
+    }
+    const[allMeme,setallMeme] = useState([])
+    useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setallMeme(data.data.memes))
+    },[])
     return(<main className={styles.main}>
-        <form className={styles.form} onSubmit={getMemeImage}>
-            <input type="text"placeholder="top text" className={styles.txt1}/>
-            <input type="text" placeholder="bottom text"className={styles.txt2} />
+         <form className={styles.form} onSubmit={getMemeImage}>
+            <input type="text"placeholder="top text" value={meme.toptext} onChange={handlechange} name="toptext" className={styles.txt1}/>
+            <input type="text" placeholder="bottom text"value={meme.bottomtext} onChange={handlechange} name="bottomtext"className={styles.txt2} />
             <button type="submit" className={styles.btn}>Get a new image  🖼</button>
         </form>
-          <img src={meme.randomImage} className={styles.random} />
+        <div className={styles.txtgenerate}>
+        <img src={meme.randomImage} className={styles.random} />
+        <h2 className={styles.toptext}>{meme.toptext}</h2> 
+        <h2 className={styles.bottomtext}>{meme.bottomtext}</h2>
+        </div>
 
     </main>)
 }
